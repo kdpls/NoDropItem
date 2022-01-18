e6a275a6d8984e2feb109cbcf309a015263167cf
KygekDev <68368066+KygekDev@users.noreply.github.com>
2022-01-19 00:55:32 +0800

    Add license, bump version and change Kygekraqmak to KygekDev

--- src/KygekDev/NoDropItem/NoDropItem.php
+++ src/KygekDev/NoDropItem/NoDropItem.php
@@ -1,8 +1,18 @@
 <?php
 
+/*
+ * Prevent players from dropping items from their inventory
+ * Copyright (C) 2020-2022 KygekDev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 declare(strict_types=1);
 
-namespace Kygekraqmak\NoDropItem;
+namespace KygekDev\NoDropItem;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
